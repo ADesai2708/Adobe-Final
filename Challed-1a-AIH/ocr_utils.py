c4f5247e886967c090196a4b48d4d20bed3d0a03
Anjali Desai <anjali_desai@srmap.edu.in>
2026-05-23 00:08:48 +0530

    final

--- Challed-1a-AIH/ocr_utils.py
+++ Challed-1a-AIH/ocr_utils.py
@@ -11,4 +11,4 @@ def extract_text_with_ocr(pdf_path, page_num):
     image = np.array(images[0])
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     ocr_result = pytesseract.image_to_string(gray, lang="eng+hin+ara+jpn")
-    return [line.strip() for line in ocr_result.split("\n") if line.strip()]
+    return [line.strip( ) for line in ocr_result.split("\n") if line.strip()]
